9db11a42e924526cf014d70d2f068fc2e1948c0f
Gabriel Jorge Coutinho <101015345+gabrieljcoutinho@users.noreply.github.com>
2025-04-08 22:37:53 -0300

    Update and rename ex18.py to 9.8-anguloSenoCoseno.py

--- 9.8-anguloSenoCoseno.py
+++ 9.8-anguloSenoCoseno.py
@@ -1,4 +1,4 @@
-'''Faça um progrma que leia um angulo e fale o seno, cosseno e a tangente'''
+#Faça um progrma que leia um angulo e fale o seno, cosseno e a tangente
 from math import radians, sin, cos, tan
 angulo = float(input('Digite um angulo qualquer: '))
 seno = sin(radians(angulo))
